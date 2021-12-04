31510fe26e0350296b500550c62ecdc452204826
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2021-12-04 14:47:45 +0100

    day4 python done

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ My attempt at Advent of Code 2021.
 | [day 1](https://adventofcode.com/2021/day/1)   |    [Python :heavy_check_mark:](./Solutions/day1/py/main.py) `Time: 0.000793 sec`| [Rust :heavy_check_mark:](./Solutions/day1/rust/src/main.rs) `Time: 0.0001488 sec` |
 | [day 2](https://adventofcode.com/2021/day/2)   |    [Python :heavy_check_mark:](./Solutions/day2/py/main.py) `Time: 0.000733 sec`   | [Rust :heavy_check_mark:](./Solutions/day2/rust/src/main.rs) `Time: 0.0001821 sec` |
 | [day 3](https://adventofcode.com/2021/day/3)   |    [Python :heavy_check_mark:](./Solutions/day3/py/main.py) `Time: 0.001374 sec`   | [Rust :heavy_check_mark:](./Solutions/day3/rust/src/main.rs) `Time: 0.0007656 sec` |
-| [day 4](https://adventofcode.com/2021/day/4)   |    [Python :x:](./Solutions/day4/py/main.py)    | [Rust :x:](./Solutions/day4/rust/src/main.rs)  |
+| [day 4](https://adventofcode.com/2021/day/4)   |    [Python :heavy_check_mark:](./Solutions/day4/py/main.py) `Time: 0.078123 sec`   | [Rust :x:](./Solutions/day4/rust/src/main.rs)  |
 | [day 5](https://adventofcode.com/2021/day/5)   |    [Python :x:](./Solutions/day5/py/main.py)    | [Rust :x:](./Solutions/day5/rust/src/main.rs)  |
 | [day 6](https://adventofcode.com/2021/day/6)   |    [Python :x:](./Solutions/day6/py/main.py)    | [Rust :x:](./Solutions/day6/rust/src/main.rs)  |
 | [day 7](https://adventofcode.com/2021/day/7)   |    [Python :x:](./Solutions/day7/py/main.py)    | [Rust :x:](./Solutions/day7/rust/src/main.rs)  |
---
+++ Solutions/day4/py/main.py
@@ -0,0 +1,90 @@
+def has_bingo(board) -> bool:
+    for row in board:
+        if all((mark for _, mark in row)):
+            return True
+    for row in zip(*board):
+        if all((mark for _, mark in row)):
+            return True
+    return False
+
+
+def mark_nums(num, board):
+    board_len = len(board)
+    for row in range(board_len):
+        for col in range(board_len):
+            _num, marked = board[row][col]
+            if _num == num:
+                board[row][col] = (_num, True)
+
+
+def sum_unmarked(board) -> int:
+    total = 0
+    for row in board:
+        total += sum([num for num, mark in row if not mark])
+    return total
+
+
+def part_a(nums: list[int], boards: list[list[list[tuple[int, bool]]]]) -> int:
+    for num in nums:
+        for board in boards:
+            mark_nums(num, board)  # inplace
+            if has_bingo(board):
+                return sum_unmarked(board) * num
+
+
+def part_b(nums: list[int], boards: list[list[list[tuple[int, bool]]]]) -> int:
+    boards_with_bingo = []
+    for num in nums:
+        for board in boards:
+            if not has_bingo(board):
+                mark_nums(num, board)  # inplace
+                if has_bingo(board):
+                    boards_with_bingo.append((board, num))
+    last_board_to_get_bingo, num = boards_with_bingo.pop()
+    return sum_unmarked(last_board_to_get_bingo) * num
+
+
+def load_bingo_board(input_str):
+    matrix: list[list[tuple[int, bool]]] = []
+    for line in input_str:
+        if line == "\n":
+            break
+        matrix.append([(int(num), False) for num in line.split(" ") if num != ""])
+    return matrix
+
+
+def reset_boards(boards):
+    for board in boards:
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                num, _ = board[row][col]
+                board[row][col] = (num, False)
+    return boards
+
+def main() -> tuple:
+    bingo_nums: list[int]
+    bingo_boards: list[list[list[tuple[int, bool]]]] = []
+
+    with open("../input.txt") as f:
+        bingo_nums = [int(num) for num in f.readline().split(",")]
+        f.readline()
+        while True:
+            matrix = load_bingo_board(f)
+            if len(matrix) == 0:
+                break
+            bingo_boards.append(matrix)
+
+    return part_a(bingo_nums, bingo_boards), part_b(bingo_nums, reset_boards(bingo_boards))
+
+
+if __name__ == '__main__':
+    import time
+
+    start_time = time.perf_counter()
+    a, b = main()
+    end_time = time.perf_counter()
+    print(f"Result part_a: {a}")
+    assert 21607, a
+    print(f"Result part_b: {b}")
+    assert 19012, b
+    print(f"Time: {(end_time - start_time):4f} sec")
