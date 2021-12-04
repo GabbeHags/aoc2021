c0bd99f50575c0808d52ffd99bfc36acb39fae81
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2021-12-04 14:48:05 +0100

    fixed bug in profiling function

--- misc/misc_funcs.py
+++ misc/misc_funcs.py
@@ -3,10 +3,11 @@ import pstats
 
 
 def profile_func(func):
-    def inner():
+    def inner(*args, **kwargs):
         profiler = cProfile.Profile()
+        print(f"Function name: {func.__name__}")
         profiler.enable()
-        result = func()
+        result = func(*args, **kwargs)
         profiler.disable()
         stats = pstats.Stats(profiler).sort_stats(pstats.SortKey.TIME)
         stats.print_stats()
